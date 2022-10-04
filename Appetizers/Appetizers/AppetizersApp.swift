9ea30f2eebc61f8a9c7ed4c071c321b231a0a4f9
RagingCODfish <zachary.uptin@me.com>
2022-10-04 12:50:08 +1000

    Added Appetizers

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Zach Uptin on 4/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
