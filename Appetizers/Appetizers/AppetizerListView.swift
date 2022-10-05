d9f4c334c58cee2342708cddfb640db87f93fe16
RagingCODfish <zachary.uptin@me.com>
2022-10-05 14:34:36 +1000

    connected AppetizerListView to the Network Manager Added AppetizerListViewModel

--- Appetizers/Appetizers/AppetizerListView.swift
+++ Appetizers/Appetizers/AppetizerListView.swift
@@ -8,27 +8,21 @@
 import SwiftUI
 
 struct AppetizerListView: View {
-    
-    @State private var appetizers: [Appetizer] = []
+    @StateObject var viewModel = AppetizerListViewModel()
+
     var body: some View {
         NavigationView {
-            List(MockData.appetizers) { appetizer in
+            List(viewModel.appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("Appetizers")
         }
-    }
-    
-    func getAppetizers() {
-        NetworkManager.shared.getAppetizers { result in
-            switch result {
-            case .success(let appetizers):
-                self.appetizers = appetizers
-            case.failure(let error):
-                print(error.localizedDescription)
-            }
+        .onAppear {
+            viewModel.getAppetizers()
         }
     }
+    
+    
 }
 
 struct AppetizerListView_Previews: PreviewProvider {
