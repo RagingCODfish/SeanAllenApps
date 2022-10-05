f89341ab175e70ba32c2658a31a2162b865fa4b9
RagingCODfish <zachary.uptin@me.com>
2022-10-05 15:25:42 +1000

    Added Alert File Warnings seem to be Xcode/ Swift 6 Error

--- Appetizers/Appetizers/Alert.swift
+++ Appetizers/Appetizers/Alert.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: String
-    let message: String
+    let title: Text
+    let message: Text
     let dismissButton: Alert.Button
 }
 
 struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input", message: "Something is wrong with the camera. We are unable to capture the input", dismissButton: .default(Text("OK")))
+    static let invalidData      = AlertItem(title: Text("Server Error"),
+                                            message: Text("SThe data recieved from the server was invalid. Please contact support"),
+                                            dismissButton: .default(Text("OK")))
     
-    static let invalidScannedType = AlertItem(title: "Invalid Scan Type", message: "The value scanned is not valid. This app scans EAN-8 and EAN-13", dismissButton: .default(Text("OK")))
+    static let invalidResponse  = AlertItem(title: Text("Server Error"),
+                                            message: Text("Invalid response from the server. Please try again later or contact support"),
+                                            dismissButton: .default(Text("OK")))
+    
+    static let invalidURL       = AlertItem(title: Text("Server Error"),
+                                            message: Text("There was an issue connecting to the server. If this persists, please contact support."),
+                                            dismissButton: .default(Text("OK")))
+    
+    static let unableToComplete = AlertItem(title: Text("Server Error"),
+                                            message: Text("Unable to complete your request at this time. Please check you internet connection and try again."),
+                                            dismissButton: .default(Text("OK")))
 }
--- Appetizers/Appetizers/AppetizerListView.swift
+++ Appetizers/Appetizers/AppetizerListView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct AppetizerListView: View {
     @StateObject var viewModel = AppetizerListViewModel()
-
+    
     var body: some View {
         NavigationView {
             List(viewModel.appetizers) { appetizer in
@@ -19,6 +19,11 @@ struct AppetizerListView: View {
         }
         .onAppear {
             viewModel.getAppetizers()
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
         }
     }
     
