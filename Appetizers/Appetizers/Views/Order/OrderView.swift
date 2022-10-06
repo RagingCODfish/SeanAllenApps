cecf5b0da3168255c0bef8c4d97f8aa48e8e5569
RagingCODfish <zachary.uptin@me.com>
2022-10-06 16:36:32 +1000

    Added OrderView, changed delete code to simplify code

--- Appetizers/Appetizers/Views/Order/OrderView.swift
+++ Appetizers/Appetizers/Views/Order/OrderView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct OrderView: View {
+    @State private var orderItems = MockData.orderItems
     var body: some View {
         NavigationView {
-            Text("OrderView")
-                .navigationTitle("Orders")
+            VStack {
+                List {
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(.plain)
+                
+                Button {
+                    print("Order Placed")
+                } label: {
+                    APButton(title: "$99.99 - Order")
+                }
+                .padding(.bottom, 25)
+            }
+            .navigationTitle("Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
