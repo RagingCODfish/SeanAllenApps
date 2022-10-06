10cad22cd732f8795c0fb9497b1ef6a5b50a9c4d
RagingCODfish <zachary.uptin@me.com>
2022-10-06 14:51:17 +1000

    Added Account View Form

--- Appetizers/Appetizers/Views/Account/AccountView.swift
+++ Appetizers/Appetizers/Views/Account/AccountView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthday = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                    DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                .autocorrectionDisabled(true)
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
+            .navigationTitle("Account")
         }
     }
 }
@@ -19,5 +47,6 @@ struct AccountView: View {
 struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
         AccountView()
+            .preferredColorScheme(.dark)
     }
 }
