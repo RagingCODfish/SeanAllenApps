4a049c280a48ee0920fdb8a0ac68f0ac5c521a15
RagingCODfish <zachary.uptin@me.com>
2022-11-22 19:32:34 +1000

    Added Guides Added Collection Added Youtube links to Guide videos

--- Appetizers/Appetizers/Views/Components/APButton.swift
+++ Appetizers/Appetizers/Views/Components/APButton.swift
@@ -2,27 +2,26 @@
 //  APButton.swift
 //  Appetizers
 //
-//  Created by Zach Uptin on 6/10/2022.
+//  Created by Zach Uptin on 18/11/2022.
 //
 
 import SwiftUI
 
 struct APButton: View {
     let title: LocalizedStringKey
-    let color: String
     var body: some View {
         Text(title)
             .font(.title3)
             .fontWeight(.semibold)
             .frame(width: 350, height: 50)
             .foregroundColor(.black)
-            .background(Color(color))
+//            .background(Color(color))
             .cornerRadius(10)
     }
 }
 
 struct APButton_Previews: PreviewProvider {
     static var previews: some View {
-        APButton(title: "Test Title", color: "Light Blue")
+        APButton(title: "Test")
     }
 }
